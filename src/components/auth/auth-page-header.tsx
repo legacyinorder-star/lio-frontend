b4d0eea4b4a832c93e2ba181b9cffe4e4585a2bf
Oduntan Balogun <15163838+odbalogun@users.noreply.github.com>
2025-06-28 22:06:58 +0100

    Updated the logo and added primary button color

--- src/components/auth/auth-page-header.tsx
+++ src/components/auth/auth-page-header.tsx
@@ -5,13 +5,13 @@ export function AuthPageHeader() {
 		<header className="w-full relative overflow-hidden">
 			<div className="w-full max-w-[2000px] mx-auto flex flex-col h-full">
 				{/* Navigation Bar */}
-				<div className="flex pt-10 px-4 md:px-6 lg:px-8 h-16">
+				<div className="flex pt-10 px-4 md:px-6 lg:px-8 h-16 mb-4">
 					<div className="flex">
 						<Link to="/" className="flex items-center">
 							<img
-								src="/logos/LIO_Logo_Color.svg"
+								src="/logos/Original.svg"
 								alt="Legacy In Order"
-								className="h-10"
+								className="h-12"
 							/>
 						</Link>
 					</div>
