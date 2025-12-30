400bf1f34755c30a35e0c617d2185a7e56aff7b0
Oduntan Balogun <15163838+odbalogun@users.noreply.github.com>
2025-12-30 12:09:36 +0100

    Added company number

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -68,7 +68,12 @@ export default function Footer({ showCTA = true }: FooterProps) {
 										<path d="M23.953 4.57a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.06a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.936 4.936 0 004.604 3.417 9.867 9.867 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0024 4.59z" />
 									</svg>
 								</a>
-								<a href="#" className="text-white/80 hover:text-white">
+								<a
+									href="https://www.instagram.com/legacyinorder/"
+									className="text-white/80 hover:text-white"
+									target="_blank"
+									rel="noopener noreferrer"
+								>
 									<svg
 										className="h-6 w-6"
 										fill="currentColor"
@@ -200,12 +205,12 @@ export default function Footer({ showCTA = true }: FooterProps) {
 								</Link>
 							</div>
 							<p className="text-[0.75rem] font-normal text-white/60 max-w-2xl">
-								© LegacyInOrder Ltd • LagacyInOrder is not a law firm and is not
-								regulated by the Solicitors Regulation Authority (SRA). Our
-								online will writing service is an online service providing legal
-								forms and information to allow you to draft your own will. Our
-								telephone service provides guidance, we do not provide bespoke
-								advice or tax advice.
+								© LegacyInOrder Ltd (Company No. 16597440) • LagacyInOrder is
+								not a law firm and is not regulated by the Solicitors Regulation
+								Authority (SRA). Our online will writing service is an online
+								service providing legal forms and information to allow you to
+								draft your own will. Our telephone service provides guidance, we
+								do not provide bespoke advice or tax advice.
 							</p>
 						</div>
 					</div>
